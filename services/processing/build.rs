797ced6ddbf47d2dc0872f864ba448d19bb487b1
aminpola2001-ctrl <aminpola2001@gmail.com>
2026-03-16 20:16:00 +0200

    Update build.rs

--- services/processing/build.rs
+++ services/processing/build.rs
@@ -3,7 +3,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // This makes the path work both locally and in CI regardless of working directory
     let manifest_dir = std::env::var("CARGO_MANIFEST_DIR")?;
     let proto_root = format!("{}/../../proto", manifest_dir);
-
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
@@ -11,17 +10,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             std::path::PathBuf::from(std::env::var("OUT_DIR")?)
                 .join("processing_descriptor.bin"),
         )
-        .compile(
+        .compile_protos(
             &[
                 &format!("{}/processing/v1/engine.proto", proto_root),
                 &format!("{}/ingestion/v1/events.proto", proto_root),
             ],
             &[&proto_root],
         )?;
-
     // Re-run build script if any proto file changes
     println!("cargo:rerun-if-changed={}/processing/v1/engine.proto", proto_root);
     println!("cargo:rerun-if-changed={}/ingestion/v1/events.proto", proto_root);
-
     Ok(())
 }
